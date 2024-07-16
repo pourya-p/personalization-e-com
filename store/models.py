4145fefa14a92be7a533c816e0c839543160604f
pourya-p <pourya.parhoode@gmail.com>
2024-07-16 10:55:44 +0330

    added product and categories model

--- store/models.py
+++ store/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100, unique=True)
+
+    class Meta:
+        ordering = ('name',)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    category = models.ForeignKey('category', on_delete=models.SET_NULL, null=True, related_name='products')
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique=True)
+    img = models.ImageField(upload_to='product_images/%Y/%m/%d/')
+    description = models.TextField()
+    price = models.IntegerField()
+    in_stock = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('-updated',)
+
+    def __str__(self):
+        return self.name
