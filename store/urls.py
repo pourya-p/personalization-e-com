d795c3fc5963c2f2a7833c6f67e6144efdf08270
pourya-p <pourya.parhoode@gmail.com>
2024-07-19 14:21:44 +0330

    create view for product list and url

--- store/urls.py
+++ store/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = 'store'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    re_path(r'/product/(?P<slug>[-\w]+)', views.IndexView.as_view(), name='index'),
+    path('product-list', views.ProductListView.as_view(), name='product_list'),
+    re_path(r'/product/(?P<slug>[-\w]+)', views.IndexView.as_view(), name='product_detail'),
 
 ]
--- store/views.py
+++ store/views.py
@@ -1,7 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, redirect, reverse
 from django.views import View
+from .forms import PersonalizeForm
+from .models import Product
 
 
 class IndexView(View):
-    def get(self, requests):
-        return render(requests, 'store/index.html')
+    form_class = PersonalizeForm
+
+    def get(self, request):
+        form = self.form_class
+        return render(request, 'store/index.html', {'form': form})
+
+
+class ProductListView(View):
+    def get(self, request):
+        products = Product.objects.filter(
+            sex=request.GET.get('sex', None),
+            age_max__gte=request.GET.get('age', None),
+            age_min__lte=request.GET.get('age', None),
+            height_max__gte=request.GET.get('height', None),
+            height_min__lte=request.GET.get('height', None),
+            weight_max__gte=request.GET.get('weight', None),
+            weight_min__lte=request.GET.get('weight', None),
+
+        )
+        return render(request, 'store/product_list.html', {'products': products})
+
