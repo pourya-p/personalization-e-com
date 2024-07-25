94d66543fac61d9d2d5b3db63d9a29b964b28c3d
pourya-p <pourya.parhoode@gmail.com>
2024-07-25 14:20:02 +0330

    create order add and made order models

---
+++ order/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from store.models import Product
+
+
+class Order(models.Model):
+    name = models.CharField(max_length=100, blank=False)
+    phone_number = models.CharField(max_length=11, blank=False, null=True)
+    address = models.CharField(max_length=500, blank=False, null=True)
+    total_price = models.PositiveIntegerField(null=True)
+    create_date = models.DateTimeField(auto_now_add=True)
+
+
+def get_sentinel_product():
+    return Product.objects.get_or_create(name="deleted")[0]
+
+
+class OrderProduct(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.SET(get_sentinel_product))
+    qty = models.PositiveSmallIntegerField()
+    price_on_order = models.PositiveIntegerField()
+
