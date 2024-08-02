f5bbdfdc834d97d69ab8240d0fcd92c500f587b3
pourya-p <pourya.parhoode@gmail.com>
2024-08-02 12:11:34 +0330

    added order to admin and make it inline

--- order/admin.py
+++ order/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import OrderProduct, Order
 
-# Register your models here.
+class ProductOrderInLine(admin.StackedInline):
+    model = OrderProduct
+    raw_id_fields = ['product']
+    extra = 0
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = [ProductOrderInLine]
+
+admin.site.register(Order, OrderAdmin)
